3b4d840e2546b35ba897023e90a85015c6e71fc3
ferris <yupferris@gmail.com>
2017-12-05 15:35:01 +0100

    What a nice little playground :)

---
+++ src/main.rs
@@ -0,0 +1,83 @@
+extern crate gl;
+extern crate glutin;
+
+use gl::types::*;
+use glutin::*;
+
+use std::ffi::CString;
+use std::mem;
+use std::ptr;
+
+fn main() {
+    if let Err(e) = do_it() {
+        println!("Awww, shoot! {}", e);
+    }
+}
+
+fn do_it() -> Result<(), String> {
+    let context = HeadlessRendererBuilder::new(0, 0)
+        .with_gl(GlRequest::Specific(Api::OpenGl, (4, 3)))
+        .build_strict()
+        .map_err(|e| format!("Could not create headless context: {}", e))?;
+
+    unsafe {
+        // Set up GL
+        context.make_current().map_err(|e| format!("Could not make context current: {}", e))?;
+
+        gl::load_with(|symbol| context.get_proc_address(symbol) as *const _);
+
+        // Create shader
+        //  TODO: Check errors etc
+        let shader = gl::CreateShader(gl::COMPUTE_SHADER);
+        let src = "
+
+#version 430
+
+layout(local_size_x = 4) in;
+
+layout(std430, binding = 0) buffer resultBuffer
+{
+    uint result[];
+};
+
+void main()
+{
+    result[gl_LocalInvocationIndex] = 42 + gl_LocalInvocationIndex;
+}
+
+        ";
+        let c_src = CString::new(src.as_bytes()).unwrap();
+        gl::ShaderSource(shader, 1, &c_src.as_ptr(), ptr::null());
+        gl::CompileShader(shader);
+
+        let program = gl::CreateProgram();
+        gl::AttachShader(program, shader);
+        gl::LinkProgram(program);
+
+        // Create result buffer
+        let mut ssbo: GLuint = 0;
+        gl::GenBuffers(1, &mut ssbo as *mut _);
+        gl::BindBuffer(gl::SHADER_STORAGE_BUFFER, ssbo);
+        gl::BufferData(gl::SHADER_STORAGE_BUFFER, mem::size_of::<[u32; 4]>() as _, ptr::null(), gl::DYNAMIC_COPY);
+        gl::BindBufferBase(gl::SHADER_STORAGE_BUFFER, 0, ssbo);
+
+        gl::UseProgram(program);
+        gl::DispatchCompute(1, 1, 1);
+
+        gl::MemoryBarrier(gl::SHADER_STORAGE_BARRIER_BIT);
+
+        // Read result
+        let res = {
+            let ptr = gl::MapBuffer(gl::SHADER_STORAGE_BUFFER, gl::READ_ONLY) as *const [u32; 4];
+            let res = *ptr;
+            gl::UnmapBuffer(gl::SHADER_STORAGE_BUFFER);
+            res
+        };
+
+        println!("Result: {:?}", res);
+
+        // TODO: Cleanup etc   
+    }
+
+    Ok(())
+}
